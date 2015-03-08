0e5ff90b6b3a08735c8fe036769c188eef47aa94
Peter Todd <peter@petertodd.com>
2015-03-08 15:38:05 +0000

    Initial Commit

---
+++ BGSCircleWIthPercentageIndicator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BGSCircleWIthPercentageIndicator
+//
+//  Created by Peter Todd Air on 08/03/2015.
+//  Copyright (c) 2015 Bright Green Star. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BGSCircleWIthPercentageIndicatorTests/BGSCircleWIthPercentageIndicatorTests.m
@@ -0,0 +1,40 @@
+//
+//  BGSCircleWIthPercentageIndicatorTests.m
+//  BGSCircleWIthPercentageIndicatorTests
+//
+//  Created by Peter Todd Air on 08/03/2015.
+//  Copyright (c) 2015 Bright Green Star. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface BGSCircleWIthPercentageIndicatorTests : XCTestCase
+
+@end
+
+@implementation BGSCircleWIthPercentageIndicatorTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
